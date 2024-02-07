56902ffe4029308802ecaa50b80d6d5c03bbfaaf
Bett <bettpatrick080@gmail.com>
2024-02-07 20:29:03 +0300

    Completed Assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,21 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+
+    console.log(`before setState: ${count}`)
+
+    // call setCount twice, to update the counter by two every time we click
+    // But still  when setCount is called the second time, count is still equal to 0!
+    // Rather than passing a new value into setCount, we can instead pass a callback function.
+    // when called inside setCount, will be passed the current state of count.
+    setCount((currentCount)=>currentCount+1);
+    console.log(`After first Increment: ${count}`)
+    
+    setCount((currentCount)=>currentCount+1);
+    console.log(`after second: ${count}`)
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button style={{fontSize:18, height:30}} onClick={increment}>I have been clicked {count} times</button>;
 }
 
 export default Counter;
